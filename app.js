78f6fad926bf4d68697c167ccae325e941b6cf75
julcavasquez <jvandersson93@gmail.com>
2025-02-13 13:15:26 -0500

    Add funcion sortear amigo y asginarTexto

--- app.js
+++ app.js
@@ -2,25 +2,30 @@
 let misAmigos = [];
 let nomAmigo ;
 var listaAmigos = document.querySelector('#listaAmigos');
-
+let num;
 
 //Funcion Agregar Amigo
 function agregarAmigo(){
 //Capturando el valor del input
     nomAmigo = document.getElementById('amigo').value;
-    console.log(nomAmigo);
-//Agregando los nombres a la lista
-    misAmigos.push(nomAmigo); 
-    mostrarAmigos();
-    limpiarCaja();
-    return;
+    if(nomAmigo == ''){
+       alert("Debe ingresar un nombre valido!");
+    }else{
+        console.log(nomAmigo);
+    //Agregando los nombres a la lista
+        misAmigos.push(nomAmigo); 
+        mostrarAmigos();
+        limpiarCaja();        
+        return;
+    }
+    
 }
 
 //Funcion mostrar lista de amigos
 function mostrarAmigos(){    
     listaAmigos.innerHTML = "";
     misAmigos.forEach((item) => {
-        listaAmigos.innerHTML += `<li>${item}</li>`;
+       asignarTexto('#listaAmigos',`<li>${item}</li>`);
     });
     console.log(misAmigos);
     return;
@@ -30,4 +35,29 @@ function mostrarAmigos(){
 //Funcion limpiar caja texto
 function limpiarCaja(){
     document.querySelector('#amigo').value = '' ;
+}
+
+
+//Funcion obtener el valor random
+function valorRandom() {
+    let number = Math.floor(Math.random()*misAmigos.length);
+    return number;
+   }
+
+//Funcion sortear amigos
+function sortearAmigo(){
+    num = valorRandom();
+    console.log(num);
+    console.log(misAmigos[num]);
+    let listaAmigosHTML = document.querySelector('#listaAmigos');
+    listaAmigosHTML.innerHTML = '';
+    asignarTexto('#resultado',`El amigo secreto sorteado es: ${misAmigos[num]}`);
+}
+
+//Funcion asignar texto a un elemento
+function asignarTexto(elemento,texto){
+    let elementoHTML = document.querySelector(elemento);
+    elementoHTML.innerHTML += texto;
+    return;
+
 }
