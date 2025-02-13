14a03edd136fb75cd692bd245574d250cb3bbf5a
julcavasquez <jvandersson93@gmail.com>
2025-02-13 11:16:20 -0500

    Add funcion mostrar lista amigos

--- app.js
+++ app.js
@@ -1,11 +1,27 @@
 // El principal objetivo de este desafío es fortalecer tus habilidades en lógica de programación. Aquí deberás desarrollar la lógica para resolver el problema.
+let misAmigos = [];
+let nomAmigo ;
+var listaAmigos = document.querySelector('#listaAmigos');
 
 
 //Funcion Agregar Amigo
 function agregarAmigo(){
-    let listaAmigos = [];
-    let nomAmigo;
 //Capturando el valor del input
     nomAmigo = document.getElementById('amigo').value;
     console.log(nomAmigo);
+//Agregando los nombres a la lista
+    misAmigos.push(nomAmigo); 
+    mostrarAmigos();
+    return;
 }
+
+//Funcion mostrar lista de amigos
+function mostrarAmigos(){    
+    listaAmigos.innerHTML = "";
+    misAmigos.forEach((item) => {
+        listaAmigos.innerHTML += `<li>${item}</li>`;
+    });
+    console.log(misAmigos);
+    return;
+}
+
